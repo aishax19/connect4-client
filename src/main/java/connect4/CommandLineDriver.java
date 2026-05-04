2c214917ea7fb11933e47f4adb8ea8ff82831c0d
Aisha Noor <anoor@knox.edu>
2026-05-04 00:25:47 -0500

    Implement Connect4 command line client with input validation and board display

--- src/main/java/connect4/CommandLineDriver.java
+++ src/main/java/connect4/CommandLineDriver.java
@@ -11,57 +11,73 @@ import connect4.response.StatusResponse;
 public class CommandLineDriver 
 {
 
-    public static void main(String[] args)
-    {
-        //TODO: you have to switch to euclid to run this on campus
-        // localhost means "the current machine", and unless you
-        // are running your own server, localhost won't work
-        //String baseUrl = "http://euclid.knox.edu:8083/connect4";
-        String baseUrl = "http://localhost:8080/connect4";
-        String user = "spacco";
-        Connector conn = new Connector(baseUrl, user);
-        
-        System.out.println("Welcome!");
-        help();
-        try (Scanner scanner = new Scanner(System.in)) {
-            while (true) {
-                String command = scanner.next();
-                if (command.equals("new")) {
-                    NewGameResponse response = conn.newGame();
-                    System.out.println(response.toString());
-                } else if (command.equals("move")) {
-                    int col = scanner.nextInt();
+public static void main(String[] args)
+{
+    String baseUrl = "http://euclid.knox.edu:8083/connect4";
+    String user = "aisha"; 
+    Connector conn = new Connector(baseUrl, user);
+    
+    System.out.println("Welcome!");
+    help();
+    try (Scanner scanner = new Scanner(System.in)) {
+        while (true) {
+            String command = scanner.next();
+            if (command.equals("new")) {
+                NewGameResponse response = conn.newGame();
+                System.out.println(response.toString());
+            } else if (command.equals("move")) {
+                // client-side validation before sending
+                if (!scanner.hasNextInt()) {
+                    System.out.println("Error: column must be a number.");
+                    scanner.next(); // discard bad input
+                    continue;
+                }
+                int col = scanner.nextInt();
+                if (col < 0 || col > 6) {
+                    System.out.println("Error: column must be between 0 and 6.");
+                    continue;
+                }
+                try {
                     MoveResponse move = conn.makeMove(col);
                     System.out.println(move);
                     if (move.gameOver) {
                         System.out.println("Game over! Winner: " + move.winner);
-                        break;
+                        System.out.println("Type 'new' to play again or 'quit' to exit.");
                     }
-                } else if (command.equals("quit")) {
-                    break;
-                } else if (command.equals("status")) {
+                } catch (connect4.response.Connect4Error e) {
+                    System.out.println("Error: " + e.getMessage());
+                }
+            } else if (command.equals("quit")) {
+                break;
+            } else if (command.equals("status")) {
+                try {
                     StatusResponse status = conn.status();
                     System.out.println(status.toString());
-                } else if (command.equals("list")) {
-                    ListStrategiesResponse list = conn.listStrategies();
-                    for (Strategy strategy : list.getStrategies()) {
-                        System.out.println(strategy.name + ": " + strategy.description);
-                    }
-                } else if (command.equals("strategy")) {
-                    String strategy = scanner.next();
+                } catch (connect4.response.Connect4Error e) {
+                    System.out.println("Error: " + e.getMessage());
+                }
+            } else if (command.equals("list")) {
+                ListStrategiesResponse list = conn.listStrategies();
+                for (Strategy strategy : list.getStrategies()) {
+                    System.out.println(strategy.name + ": " + strategy.description);
+                }
+            } else if (command.equals("strategy")) {
+                String strategy = scanner.next();
+                try {
                     conn.setStrategy(strategy);
                     System.out.println("Strategy set to " + strategy);
-                } else if (command.equals("help")) {
-                    help();
-                } else {
-                    System.out.println("Unknown command: " + command);
+                } catch (connect4.response.Connect4Error e) {
+                    System.out.println("Error: " + e.getMessage());
                 }
-
-
+            } else if (command.equals("help")) {
+                help();
+            } else {
+                System.out.println("Unknown command: " + command + ". Type 'help' for options.");
             }
         }
-        System.out.println("Goodbye");
     }
+    System.out.println("Goodbye");
+}
 
     private static void help() {
         System.out.println("new - start a new game");
--- src/main/java/connect4/response/Util.java
+++ src/main/java/connect4/response/Util.java
@@ -2,14 +2,19 @@ package connect4.response;
 
 public class Util {
     public static String boardToString(int[][] board) {
-        StringBuilder sb = new StringBuilder();
-        for (int r = 0; r < board.length; r++) {
-            for (int c = 0; c < board[r].length; c++) {
-                sb.append(board[r][c]).append(" ");
-            }
-            sb.append("\n");
+    StringBuilder sb = new StringBuilder();
+    sb.append("  0 1 2 3 4 5 6\n");
+    sb.append("  -------------\n");
+    for (int r = 0; r < board.length; r++) {
+        sb.append("| ");
+        for (int c = 0; c < board[r].length; c++) {
+            char cell = board[r][c] == 1 ? 'X' : board[r][c] == 2 ? 'O' : '.';
+            sb.append(cell).append(" ");
         }
-        return sb.toString();
+        sb.append("|\n");
     }
+    sb.append("  -------------\n");
+    return sb.toString();
+}
 
 }
